89ce706ab82a423f361f50adcd15347e04e62028
Estif017 <estifoteklearegay@gmail.com>
2020-09-09 21:54:31 -0700

    It's done.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const quoteContainer = document.getElementById('quote-container');
+const twitterBtn = document.getElementById('tweeter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+loading(false, true);
 //getting quote from api
 async function getQuote() {
 	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -6,10 +13,38 @@ async function getQuote() {
 	try {
 		const response = await fetch(apiUrl);
 		const data = await response.json();
-		console.log(data);
+		// quoteContainer.style.display = 'block';
+		// loader.style.display = 'none';
+		loading(true, false);
+		quote.innerText = data.quoteText;
+		if (data.quoteAuthor === '') {
+			author.innerText = 'Unknown';
+		} else {
+			author.innerText = data.quoteAuthor;
+		}
+		if (data.quoteText.length > 120) {
+			quote.classList.add('long-quote');
+		} else {
+			quote.classList.remove('long-quote');
+		}
 	} catch (err) {
-		// getQuote();
-		console.log('Whoops, no quote today', err);
+		getQuote();
+		loading(false, true);
 	}
 }
 getQuote();
+
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+function tweetQuote() {
+	const tweetQuote = quote.innerText;
+	const tweetAuthor = author.innerText;
+	const tweetUrl = `https://twitter.com/intent/tweet?text=${tweetQuote} - ${tweetAuthor}`;
+	window.open(tweetUrl, '_blank');
+}
+
+function loading(isloading, ishidden) {
+	loader.hidden = isloading;
+	quoteContainer.hidden = ishidden;
+}
